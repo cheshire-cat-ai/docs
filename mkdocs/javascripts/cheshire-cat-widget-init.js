2ac6d451b7813a6769b2940bd52b0972fcd472aa
Piero Savastano <piero.savastano@gmail.com>
2024-09-03 20:19:13 +0200

    talk to the real docucat

--- mkdocs/javascripts/cheshire-cat-widget-init.js
+++ mkdocs/javascripts/cheshire-cat-widget-init.js
@@ -2,6 +2,7 @@
 
 var customHtml = 
     `<div id="cheshire-cat-widget"
-        data-url="http://localhost:1865"
+        data-url="https://docucat.cheshirecat.ai"
+        data-credential="34jhlkj3h5XCXCV!dfdfgdgV..jh5g34j5hg345kjEg5kjh35gk"
     >`;
 document.body.insertAdjacentHTML('beforeend', customHtml);
